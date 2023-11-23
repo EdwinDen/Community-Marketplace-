a519bbc2d8062bd9c626d4a1f27e18c94670b217
POLLARD1145 <pollardsamba1@gmail.com>
2023-11-23 22:15:10 +0200

    added the mvc template to our project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Community_Marketplace_.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
